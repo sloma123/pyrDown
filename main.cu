2f3f73b9b1c5690e01455f049c56a3e517d6c492
sloma123 <aleksandra.slomskaa@gmail.com>
2026-01-18 22:54:47 +0000

    gaussian blur beginning

--- main.cu
+++ main.cu
@@ -1,16 +1,35 @@
 #include <iostream>
 #include <cuda_runtime.h>
 
-int main() {
-    int deviceCount = 0;
-    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
 
-    if (deviceCount == 0) {
-        std::cout << "nie wykryto" << std::endl;
-        return 0;
+__global__ void gaussian_blur(int width, int height, unsigned char* input, unsigned char* output){
+    int pixel_col = blockIdx.x * blockDim.x + threadIdx.x;
+    int pixel_row = blockIdx.y * blockDim.y + threadIdx.y;
+    int window_size = 2
+    if (pixel_col < width && pixel_row < height) {
+        //zmienimy wyzerowanie na rozwijanie/lustro
+        float pixel_sum = 0.0f;
+        int pixel_count = 0;
+
+        for (int blur_row = -window_size; blur_row <= window_size; blur_row++) {
+            for (int blur_col = -window_size; blur_col <= window_size; blur_col++) {
+                //tu trzeba przemnożyć przez macierz może zmienić pętle
+            }
+        }
+
     }
+}
 
-    std::cout << "wykryto " << deviceCount  << std::endl;
+
+int main() {
+    
+    gaussian_blur<<<blocks, threads>>>(d_input, d_output, WIDTH, HEIGHT, BLUR_SIZE);
+    //cuda errors
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("Kernel launch error: %s\n", cudaGetErrorString(err));
+        return 1;
+    }
 
     return 0;
 }
